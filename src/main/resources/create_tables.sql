34634424f865eac53a01c46a67789e6a6ee34a0f
Oleksandr Maister <maister.alexander@gmail.com>
2019-07-31 13:23:56 +0300

    Change create and drop table scripts

--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -24,13 +24,41 @@ CREATE TABLE IF NOT EXISTS SUBJECTS (
 	subject VARCHAR(255) NOT NULL
 );
 
--- 5
+-- 5 
+CREATE TABLE IF NOT EXISTS SCHEDULE (
+	schedule_id SERIAL NOT NULL,
+	description VARCHAR(255),
+	PRIMARY KEY (schedule_id)
+);
+
+-- 6 Weak entity
+CREATE TABLE IF NOT EXISTS MONTHLYSCHEDULE (
+	monthlyschedule_id SERIAL NOT NULL,
+	schedule_id INTEGER REFERENCES SCHEDULE (schedule_id) ON DELETE CASCADE,
+	description VARCHAR(255),
+	PRIMARY KEY (monthlyschedule_id, schedule_id)
+);
+
+-- 7 Weak entity
+CREATE TABLE IF NOT EXISTS DAILYSCHEDULE (
+	dailyschedule_id SERIAL NOT NULL,
+	monthlyschedule_id INTEGER NOT NULL,
+	schedule_id INTEGER NOT NULL,
+	description VARCHAR(255),
+	PRIMARY KEY (dailyschedule_id, monthlyschedule_id, schedule_id),
+	CONSTRAINT fk_dailyschedule
+		FOREIGN KEY (monthlyschedule_id, schedule_id) 
+		REFERENCES MONTHLYSCHEDULE (monthlyschedule_id, schedule_id) 
+		ON DELETE CASCADE
+);
+
+-- 8
 CREATE TABLE IF NOT EXISTS CAMPUS (
 	campus_id SERIAL PRIMARY KEY,
 	campus VARCHAR(255) NOT NULL
 );
 
--- 6 Weak entity
+-- 9 Weak entity
 CREATE TABLE IF NOT EXISTS CLASSROOM (
 	campus_id INTEGER NOT NULL,
 	roomnumber VARCHAR(255) NOT NULL,
@@ -39,7 +67,7 @@ CREATE TABLE IF NOT EXISTS CLASSROOM (
 	FOREIGN KEY (campus_id) REFERENCES CAMPUS (campus_id) ON DELETE CASCADE
 );
 
--- 7
+-- 10
 CREATE TABLE IF NOT EXISTS LESSON (
 	lesson_id SERIAL NOT NULL,
 	campus_id INTEGER NOT NULL,
@@ -47,38 +75,17 @@ CREATE TABLE IF NOT EXISTS LESSON (
 	beginTime TIMESTAMP,
 	endTIme TIMESTAMP,
 	subject_id INTEGER REFERENCES SUBJECTS (subject_id),
-	PRIMARY KEY(lesson_id),
-	CONSTRAINT fk_lesson 
-		FOREIGN KEY (campus_id, roomnumber) 
-		REFERENCES CLASSROOM (campus_id, roomnumber) 
-		ON DELETE CASCADE
-);
-
--- 8
-CREATE TABLE IF NOT EXISTS DAILYSCHEDULE (
-	dailyschedule_id SERIAL PRIMARY KEY,
-	description VARCHAR(255),
-	lesson_id INTEGER REFERENCES LESSON(lesson_id)
-);
-
--- 9 Weak entity
-CREATE TABLE IF NOT EXISTS MONTHLYSCHEDULE (
-	monthlyschedule_id SERIAL NOT NULL,
-	dailyschedule_id INTEGER REFERENCES DAILYSCHEDULE (dailyschedule_id) ON DELETE CASCADE,
-	description VARCHAR(255),
-	PRIMARY KEY (monthlyschedule_id, dailyschedule_id)
-);
-
--- 10 Weak entity
-CREATE TABLE IF NOT EXISTS SCHEDULE (
-	schedule_id SERIAL NOT NULL,
 	dailyschedule_id INTEGER NOT NULL,
 	monthlyschedule_id INTEGER NOT NULL,
-	description VARCHAR(255),
-	PRIMARY KEY (schedule_id),
-	CONSTRAINT fk_schedule
-		FOREIGN KEY (dailyschedule_id, monthlyschedule_id) 
-		REFERENCES MONTHLYSCHEDULE (dailyschedule_id, monthlyschedule_id) 
+	schedule_id INTEGER NOT NULL,
+	PRIMARY KEY(lesson_id),
+	CONSTRAINT fk_classroom 
+		FOREIGN KEY (campus_id, roomnumber) 
+		REFERENCES CLASSROOM (campus_id, roomnumber) 
+		ON DELETE CASCADE,
+	CONSTRAINT fk_schedule 
+		FOREIGN KEY (dailyschedule_id, monthlyschedule_id, schedule_id) 
+		REFERENCES DAILYSCHEDULE (dailyschedule_id, monthlyschedule_id, schedule_id) 
 		ON DELETE CASCADE
 );
 
@@ -93,12 +100,11 @@ CREATE TABLE IF NOT EXISTS PERSON (
 
 -- 12
 CREATE TABLE IF NOT EXISTS TEACHER (
-  teacher_id INTEGER NOT NULL,
+  teacher_id INTEGER REFERENCES PERSON (person_id)  ON DELETE CASCADE,
   salary INTEGER,
   position_id INTEGER NOT NULL,
   teacherSchedule_id INTEGER NOT NULL,
   PRIMARY KEY (teacher_id),
-  FOREIGN KEY (teacher_id) REFERENCES PERSON (person_id) ON DELETE CASCADE,
   FOREIGN KEY (position_id) REFERENCES POSITIONS (position_id),
   FOREIGN KEY (teacherSchedule_id) REFERENCES SCHEDULE (schedule_id)
 );
@@ -114,15 +120,8 @@ CREATE TABLE IF NOT EXISTS STUDENT (
   FOREIGN KEY (group_id) REFERENCES GRUP (group_id),
   FOREIGN KEY (studentSchedule_id) REFERENCES SCHEDULE (schedule_id)
  );
-
--- 14 
-CREATE TABLE IF NOT EXISTS GROUP_HAS_STUDENT (
-	group_id INTEGER REFERENCES GRUP (group_id) ON DELETE CASCADE,
-	student_id INTEGER REFERENCES STUDENT (student_id) ON DELETE CASCADE,
-	PRIMARY KEY (student_id, group_id)
-);
-
--- 15 
+ 
+ -- 14 
 CREATE TABLE IF NOT EXISTS TEACHER_SUBJECT (
 	teacher_id INTEGER REFERENCES TEACHER (teacher_id) ON DELETE CASCADE,
 	subject_id INTEGER REFERENCES SUBJECTS (subject_id) ON DELETE CASCADE,
