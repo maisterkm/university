ed03131e15d6cf263dcc13cd480f78fbdb6bde36
Oleksandr Maister <maister.alexander@gmail.com>
2019-08-01 21:25:01 +0300

    Change enum Campus to class Campus

--- src/main/java/ua/com/foxminded/university/domain/entity/Campus.java
+++ src/main/java/ua/com/foxminded/university/domain/entity/Campus.java
@@ -1,5 +1,24 @@
 package ua.com.foxminded.university.domain.entity;
 
-public enum Campus {
-    CAMPUS_A, CAMPUS_B, CAMPUS_C
+//public enum Campus {
+//    CAMPUS_A, CAMPUS_B, CAMPUS_C
+//}
+
+public class Campus {
+    private int id;
+    private String campus;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCampus() {
+        return campus;
+    }
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+    
+    
 }
