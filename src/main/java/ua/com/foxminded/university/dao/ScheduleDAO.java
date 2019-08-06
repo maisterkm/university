2b6053ffb9648800445413cd8767f0621fec0c64
Oleksandr Maister <maister.alexander@gmail.com>
2019-08-07 00:43:14 +0300

    Add interface ScheduleDAO and class ScheduleService

---
+++ src/main/java/ua/com/foxminded/university/dao/ScheduleDAO.java
@@ -0,0 +1,17 @@
+package ua.com.foxminded.university.dao;
+
+public interface ScheduleDAO {
+ // create
+//    void add(Schedule schedule) throws SQLException;
+
+    // read
+//    List<Schedule> getAll() throws SQLException;
+
+//    Schedule getById(Integer id) throws SQLException;
+
+    // update
+//    void update(Schedule schedule) throws SQLException;
+
+    // delete
+//    void remove(Schedule schedule) throws SQLException;
+}
---
+++ src/main/java/ua/com/foxminded/university/dao/service/ScheduleService.java
@@ -0,0 +1,7 @@
+package ua.com.foxminded.university.dao.service;
+
+import ua.com.foxminded.university.dao.ScheduleDAO;
+
+public class ScheduleService implements ScheduleDAO {
+
+}
