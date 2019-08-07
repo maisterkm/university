79fb1737438ae5801b2dbae23bd242971d132bed
Oleksandr Maister <maister.alexander@gmail.com>
2019-08-07 11:21:04 +0300

    Add interface MonthlyScheduleDAO and class MonthlyScheduleService

---
+++ src/main/java/ua/com/foxminded/university/dao/MonthlyScheduleDAO.java
@@ -0,0 +1,21 @@
+package ua.com.foxminded.university.dao;
+
+import java.sql.SQLException;
+
+import ua.com.foxminded.university.domain.MonthlySchedule;
+
+public interface MonthlyScheduleDAO {
+    // create
+    void add(MonthlySchedule monthlySchedule) throws SQLException;
+
+    // read
+//    List<MonthlySchedule> getAll() throws SQLException;
+
+//    MonthlySchedule getById(Integer id) throws SQLException;
+
+    // update
+//    void update(MonthlySchedule monthlySchedule) throws SQLException;
+
+    // delete
+//    void remove(MonthlySchedule monthlySchedule) throws SQLException;
+}
---
+++ src/main/java/ua/com/foxminded/university/dao/service/MonthlyScheduleService.java
@@ -0,0 +1,7 @@
+package ua.com.foxminded.university.dao.service;
+
+import ua.com.foxminded.university.dao.MonthlyScheduleDAO;
+
+public class MonthlyScheduleService implements MonthlyScheduleDAO {
+
+}
